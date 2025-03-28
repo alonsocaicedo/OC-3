07e660c1a40af4627d68ddfc45b0a68e3c51ffe4
Alonso Caicedo <alonsocaicedo038@gmail.com>
2025-03-28 13:01:37 -0400

    fetchworks function

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,1 +1,37 @@
 console.log("Hello world");
+
+//fetch("http://localhost:5678/api/works")
+//	.then((response) => {
+//		if (!response.ok) {
+//			throw new Error("Could not fetch");
+//		}
+//
+//		return response.json();
+//	})
+//
+//	.then((data) => console.log(data))
+//	.catch((error) => console.error(error));// 
+
+
+fetchWorks();
+
+    async function fetchWorks(){
+try {
+const response = await fetch("ttp://localhost:5678/api/works");
+
+if(!response.ok){
+throw new Error ("Could not fetch resource");
+
+}
+
+const data = await response.json();
+console.log(data;
+
+}
+catch(error){
+    console.error(error);
+}
+
+    }
+
+    const gallery = document.getElementById('gallery');

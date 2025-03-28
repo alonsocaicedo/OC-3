a74aacbc2bb02b9b2251979947c79292e1cc820c
Alonso Caicedo <alonsocaicedo038@gmail.com>
2025-03-28 15:10:11 -0400

    Categories fetch render functions and categories div html

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -10,28 +10,73 @@ console.log("Hello world");
 //	})
 //
 //	.then((data) => console.log(data))
-//	.catch((error) => console.error(error));// 
+//	.catch((error) => console.error(error));//
 
+async function fetchWorks() {
+	try {
+		const response = await fetch("http://localhost:5678/api/works");
 
-fetchWorks();
+		if (!response.ok) {
+			throw new Error("Could not fetch resource");
+		}
 
-    async function fetchWorks(){
-try {
-const response = await fetch("ttp://localhost:5678/api/works");
-
-if(!response.ok){
-throw new Error ("Could not fetch resource");
-
+		const data = await response.json();
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-const data = await response.json();
-console.log(data;
+const gallery = document.getElementById("gallery");
+const categoriesDiv = document.getElementById("categoriesDiv");
 
+//Display works
+// Loop thru our data
+// Create html elements
+// Put corresponding data into elements
+// Put html elements into gallery
+
+async function renderWorks() {
+	const works = await fetchWorks();
+	works.forEach((work) => {
+		const figure = document.createElement("figure");
+		const figCaption = document.createElement("figcaption");
+		const img = document.createElement("img");
+
+		figCaption.textContent = work.title;
+		img.src = work.imageUrl;
+		img.alt = work.title;
+		figure.appendChild(img);
+		figure.appendChild(figCaption);
+		gallery.appendChild(figure);
+	});
 }
-catch(error){
-    console.error(error);
+renderWorks();
+
+async function fetchCategories() {
+	try {
+		const response = await fetch("http://localhost:5678/api/categories");
+		if (!response.ok) {
+			throw new Error("Could not fetch resource");
+		}
+
+		const data = await response.json();
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-    }
-
-    const gallery = document.getElementById('gallery');
+async function renderCategories() {
+	const categories = await fetchCategories();
+	const categoriesDiv = document.getElementById("categoriesDiv");
+	categories.forEach((category) => {
+		debugger;
+		const categoryButton = document.createElement("button");
+		categoryButton.textContent = category.name;
+		categoriesDiv.appendChild(categoryButton);
+	});
+}
+renderCategories();

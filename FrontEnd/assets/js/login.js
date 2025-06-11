50d24892af3a5acb8c54232cb2c9fcb1d8fd880f
Alonso Caicedo <alonsocaicedo038@gmail.com>
2025-06-11 13:33:41 -0400

    login header and styling

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -5,13 +5,37 @@ const submitButton = document.getElementById("submit-button");
 const passwordErrorMessage = document.getElementById("password-error-message");
 const usernameErrorMessage = document.getElementById("username-error-message");
 
-loginForm.addEventListener("submit", (event) => {
+loginForm.addEventListener("submit", logUserIn);
+async function logUserIn() {
 	event.preventDefault();
-	console.log("Username:" + usernameInput.value);
-	console.log("Password:" + passwordInput.value);
-	console.log(event);
-	event.preventDefault();
-});
+	const loginData = {
+		email: usernameInput.value,
+		password: passwordInput.value,
+	};
+
+	try {
+		const response = await fetch("http://localhost:5678/api/users/login", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(loginData),
+		});
+
+		if (!response.ok) {
+			alert("Incorrect email or password");
+			throw new Error("Incorrect email or password");
+		}
+
+		const data = await response.json();
+		console.log(data);
+		// return data;
+		window.location.replace("index.html");
+	} catch (error) {
+		console.error(error);
+	}
+}
 
 usernameInput.addEventListener("blur", ($event) => {
 	if ($event.target.value.length <= 0) {
